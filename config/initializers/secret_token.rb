e00a91ae8cd831173fa55f19049a14d2f31e164d
XieGang <xiegang@zhiyisoft.com>
2013-04-28 15:45:38 +0800

    创建 member rails程序

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Member::Application.config.secret_token = '18b9cb3b32ebd82fbb1304a7c704717d0965476b480c9844171dcef2e02d74c065bc4635e96d752ed350bb66f9aaa6b452655e885dc0b18d31979e0fb27fef8c'
