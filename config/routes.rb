f04e05be98a76a1b24853815172a7f17612ce1ba
尹常鑫 <ycxxww@gmail.com>
2013-05-08 11:34:48 +0800

    添加相关路由

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Member::Application.routes.draw do
   resources :users,:only => [:create,:edit,:update] do
     collection do
       get "change_password"
+      get "forgot_password"
+      post "reset_password"
+    end
+    member do
       put "update_password"
+      get "set_new_password"
     end
   end
 
