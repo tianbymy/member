fc28108afd5fedca024a55c9ff2d39ae2ba35bb9
尹常鑫 <ycxxww@gmail.com>
2013-05-10 16:57:04 +0800

    添加路由，主页

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Member::Application.routes.draw do
     collection do
       get "change_password"
       get "forgot_password"
+      get "set_new_password"
       post "send_reset_password_email"
     end
     member do
+      get "lock"
       put "update_password"
-      get "set_new_password"
       get 'reset_password'
     end
   end
-match '/logout' => 'application#logout'
 end
