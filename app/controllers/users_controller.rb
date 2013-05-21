9c9093c97bf1aa3c9e5563fa7d211c3fa9d799df
尹常鑫 <ycxxww@gmail.com>
2013-05-21 16:01:01 +0800

    当前用户

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class UsersController < ApplicationController
   before_filter CASClient::Frameworks::Rails::Filter, only: [:index,:update_info,:change_password,:edit,:reset_password]
-  before_filter :current_user, only: [:change_password,:edit_user]
+  before_filter :current_user
   before_filter :find_ldap_by_login, only: [:update_info,:edit,:destroy,:reset_password,:update_password]
   before_filter :authorize_admin, only: [:index, :destroy]
   
