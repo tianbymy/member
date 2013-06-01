68043c7d65ab20c667e23dc31e7190e517a291fe
尹常鑫 <ycxxww@gmail.com>
2013-06-01 09:37:41 +0800

    在seed里面添加导出excel方法

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
-
+# encoding: utf-8
+require 'csv'
 account = Account::Account.get_accout
-account.each do |x|
-  a = rand(999999)
-  User.create!(login: x["login"], sn:  x["login"] , cn:  x["login"], name:  x["login"], mail: x["email"], mobile: 13800000000 ,password_confirmation: a ,password: a)
-  p "#{x['login']},#{x['login']},#{x['login']},#{x['login']},#{x['email']},13800000000,#{a}"
+header = ['登录名','姓','名','姓名','邮箱','电话','密码']
+CSV.open("db/file.csv", "wb") do |csv|
+  csv << header
+  account.each do |x|
+    a = rand(999999)
+    row = []
+    user = User.new({login: x["login"], sn:  x["login"] , cn:  x["login"], name:  x["login"], mail: x["email"], mobile: 13800000000 ,password_confirmation: a ,password: a})
+    # user.create_ldap   这一句代码才是创建用户到ldap中
+    p "#{x['login']},#{x['login']},#{x['login']},#{x['login']},#{x['email']},13800000000,#{a}"
+    row << user.login
+    row << user.sn
+    row << user.cn
+    row << user.name
+    row << user.mail
+    row << user.mobile
+    row << user.password
+    csv << row if row.size > 0
+  end
 end
-
